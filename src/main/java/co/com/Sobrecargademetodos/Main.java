6e09e9db615180685908b38415c86466a8e64269
Ervingarcia13 <ervingarcia13@hotmail.com>
2023-02-01 05:30:02 -0500

    prueba

---
+++ src/main/java/co/com/Sobrecargademetodos/Main.java
@@ -0,0 +1,12 @@
+package co.com.Sobrecargademetodos;
+
+public class Main {
+    public static void main(String[] args) {
+        Sobrecargademetodos sobrecargademetodos = new Sobrecargademetodos();
+
+        int resultado = sobrecargademetodos.suma(10,20);
+        System.out.println(" El resultado es: "  +resultado );
+
+
+    }
+}
---
+++ src/main/java/co/com/Sobrecargademetodos/Sobrecargademetodos.java
@@ -0,0 +1,20 @@
+package co.com.Sobrecargademetodos;
+
+public class Sobrecargademetodos {
+
+   //Enteros se colocan metodos diferentes
+    public int suma( int  valor1, int valor2){
+        return valor1+valor2;
+
+    }
+ // Flotantes
+    public float suma( float   valor1,  float valor2){
+        return valor1+valor2;
+
+}
+
+  // Doubles
+    public double suma(  double  valor1,  double valor2){
+        return valor1+valor2;
+    }
+}
